3b73764333eda93c43223f1c3cd6ddbb380dcb1a
deliri019 <deliri019@projectcyberpunk.net>
2013-04-17 11:13:48 -0300

    Adding code with functions to crypt/decrypt messages and send via netcat

---
+++ cryptdecrypt.c
@@ -0,0 +1,10 @@
+char *crypt(chrar *, char *key)
+char *decrypt(chrar *, char *key)
+
+main() {
+  croypted = crypt("hooray", "BC45A23");
+
+  send(crypted, 192.168.6.69);
+  system("nc 1234");
+  printf("decrypted = %s\n", decrypt(crypted, "BC45A23");
+}
