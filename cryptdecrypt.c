62304d59cbbe30a6d5ed10ca2433ff7623cca7be
deliri019 <deliri019@projectcyberpunk.net>
2013-04-17 19:34:49 -0300

    Some changes in cryptdecrypt.c

--- cryptdecrypt.c
+++ cryptdecrypt.c
@@ -1,10 +1,11 @@
-char *crypt(chrar *, char *key)
-char *decrypt(chrar *, char *key)
+//Definir AES Key 256? 
+
+char *crypt(char *, char *key)
+char *decrypt(char *, char *key)
 
 main() {
   croypted = crypt("hooray", "BC45A23");
-
-  send(crypted, 192.168.6.69);
+  send(crypted, 127.0.0.1);
   system("nc 1234");
   printf("decrypted = %s\n", decrypt(crypted, "BC45A23");
 }
