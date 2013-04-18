3ed7fca4beb3976198155eb01aa81c4384668b5d
Gabriel Carvalho <gabriel.carvalho@hp.com>
2013-04-18 16:43:07 -0300

    Deleting some repos that will not be used.

--- cryptdecrypt.c
+++ cryptdecrypt.c
@@ -5,7 +5,8 @@ char *decrypt(chrar *, char *key);
 
 main() {
   crypted = crypt("hooray", "BC45A23");
-  send(crypted, 192.168.6.69);
-  system("nc 1234");
+  //send(crypted, 192.168.6.69);
+  //system("nc 1234");
+
   printf("decrypted = %s\n", decrypt(crypted, "BC45A23");
 }
