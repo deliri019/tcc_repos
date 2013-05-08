5061a3ac0dd3ac825e4c02f6683cc7f0b50c4b68
deliri019 <deliri019@projectcyberpunk.net>
2013-05-08 20:41:26 -0300

    Moving back to working codes.

--- aescryptdecript.c
+++ aescryptdecript.c
@@ -4,57 +4,57 @@
 #include <readline/readline.h>
 
 static const unsigned char key[] = {
-	0x00, 0x91, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
-	0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff,
-	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
-	0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
+0x00, 0x91, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
+0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff,
+0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
 };
 
 int main()
 {
-	char *text;
-	unsigned char *enc_text = malloc(800 * sizeof(char));
-	unsigned char *dec_text = malloc(800 * sizeof(char));
+char *text;
+unsigned char *enc_text = malloc(800 * sizeof(char));
+unsigned char *dec_text = malloc(800 * sizeof(char));
 
-	text = readline("Insert the message you wish to encrypt:\t");
+text = readline("Insert the message you wish to encrypt:\t");
 
-	if (!text) {
-		printf("Error");
-	}
+if (!text) {
+printf("Error");
+}
 
     AES_KEY aeskey;
 
-	AES_set_encrypt_key(key, 256, &aeskey);
-	AES_encrypt(text, enc_text, &aeskey);
+AES_set_encrypt_key(key, 256, &aeskey);
+AES_encrypt(text, enc_text, &aeskey);
 
-	AES_set_decrypt_key(key, 256, &aeskey);
-	AES_decrypt(enc_text, dec_text, &aeskey);
+AES_set_decrypt_key(key, 256, &aeskey);
+AES_decrypt(enc_text, dec_text, &aeskey);
 
-	int x;
+int x;
 
-	printf("Original Message (string):\t");
-	printf("%s ",text);
+printf("Original Message (string):\t");
+printf("%s ",text);
 
-	printf("\nOriginal Message (hexa):\t");
-	for (x=0;*(text+x)!=0x00; x++)
-		printf("%2.X ",*(text+x));
+printf("\nOriginal Message (hexa):\t");
+for (x=0;*(text+x)!=0x00; x++)
+printf("%2.X ",*(text+x));
 
-	printf("\nEncrypted Message (hexa):\t");
-	for (x = 0; *(enc_text+x) != 0x00; x++)
-		printf("%2.X ",*(enc_text+x));
+printf("\nEncrypted Message (hexa):\t");
+for (x = 0; *(enc_text+x) != 0x00; x++)
+printf("%2.X ",*(enc_text+x));
 
-	printf("\nDecrypted Message (hexa):\t");
-	for (x=0;*(dec_text+x)!=0x00; x++)
-		printf("%2.X ",*(dec_text+x));
+printf("\nDecrypted Message (hexa):\t");
+for (x=0;*(dec_text+x)!=0x00; x++)
+printf("%2.X ",*(dec_text+x));
 
-	printf("\n");
+printf("\n");
 
-	if (dec_text != NULL)
-		free(dec_text);
-	if (enc_text != NULL)
-		free(enc_text);
-	if (text != NULL)
-		free(text);
+if (dec_text != NULL)
+free(dec_text);
+if (enc_text != NULL)
+free(enc_text);
+if (text != NULL)
+free(text);
 
-	return 0;
+return 0;
 }
