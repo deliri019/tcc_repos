d8f080dfe7a7367a704806142dfbf5fed29f6421
Gabriel <nihongabriel@gmail.com>
2013-05-27 15:31:54 -0300

    Fix na cripto por partes (first draft)

--- aescryptdecript.c
+++ aescryptdecript.c
@@ -10,15 +10,63 @@ static const unsigned char key[] = {
 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
 };
 
+AES_KEY aeskey;
+
+void print_hex(unsigned char *buffer) {
+	for (;*buffer != 0x00; buffer++)
+		printf("%2.X ",*buffer);
+	printf("\n");
+}
+
+char *crypt_buff(unsigned char *buffer) {
+	int len;
+	int cbuff_len;
+	int x;
+	unsigned char *cbuff;
+
+	len = strlen(buffer);
+
+	cbuff_len = len / 16;
+	cbuff_len = (float)cbuff_len < ((float)len / 16) ? cbuff_len + 1 : cbuff_len;
+	cbuff_len *= 16;
+
+	cbuff = NULL;
+	cbuff = malloc(cbuff_len);
+
+	memset(cbuff, 0, cbuff_len);
+
+	for (x = 0; x < cbuff_len; x += 16) {
+		AES_encrypt(buffer, cbuff, &aeskey);
+	}
+
+	return cbuff;
+}
+
+char *decrypt_buff(unsigned char *buffer) {
+	int dbuff_len;
+	int x;
+	unsigned char *dbuff;
+
+	dbuff_len = strlen(buffer);
+
+	dbuff = malloc(dbuff_len);
+
+	memset(dbuff, 0, dbuff_len);
+
+	for (x = 0; x < dbuff_len; x += 16) {
+		AES_decrypt(buffer, dbuff, &aeskey);
+	}
+
+	return dbuff;
+}
+
 int main()
 {
 	char *text;
-	unsigned char *msg_cifrada = malloc(800 * sizeof(char));
-	unsigned char *msg_decifrada = malloc(800 * sizeof(char));
+	unsigned char *msg_cifrada;
+	unsigned char *msg_decifrada;
 
 	int textlen, x, tmp;
-	unsigned char *enc_text = malloc(800 * sizeof(char));
-	unsigned char *dec_text = malloc(800 * sizeof(char));
 
 	text = readline("Insert the message you wish to encrypt:\t");
 
@@ -30,102 +78,28 @@ int main()
 
 	printf("\n####### Tamanho do texto: %d #######\n", textlen);
 
-    AES_KEY aeskey;
 
 	AES_set_encrypt_key(key, 256, &aeskey);
-//	AES_encrypt(text, enc_text, &aeskey);
-
 	AES_set_decrypt_key(key, 256, &aeskey);
-	AES_decrypt(enc_text, dec_text, &aeskey);
 
-/*
- * * Start loop to encrypt/decrypt
- */
-	printf("\nOriginal Message (string) FULL:\t");
-	printf("%s ",text);
+	msg_cifrada = crypt_buff(text);
+	msg_decifrada = decrypt_buff(msg_cifrada);
 
-	printf("\nOriginal Message (hexa) FULL:\t");
-	for (x=0;*(text+x)!=0x00; x++)
-		printf("%2.X ",*(text+x));
-
-	//Loop to encrypt 16 to 16 bytes
-	x=0;
-	tmp=textlen;
-   /*do {
-		if (tmp < 16 && tmp > 0) {
-			AES_encrypt(text+x, enc_text, &aeskey);
-	        memcpy(&msg_cifrada[x], enc_text, tmp);
-			tmp--;
-			x++;
-			printf("\n####Entrei no loop final#####\t");
-			printf("\n#MsgCifrada###%2.X####\t",msg_cifrada[x]);
-			printf("\n###valor de x = %d###\n", x);
-		} else if (tmp >= 16) {
-			AES_encrypt(text+x, enc_text, &aeskey);
-			memcpy(&msg_cifrada[x], enc_text, 16);
-			tmp-=16;
-			x++;
-			printf("\n####Entrei 1 vez no loop de 16####\t");
-			printf("\n#MsgCifrada###%2.X####\t",msg_cifrada[x]);
-			printf("\n###valor de x = %d###\n", x);
-		}
-	} while (tmp > 0);*/
-    do {
-			AES_encrypt(text+x, enc_text+x, &aeskey);
-	        memcpy(&msg_cifrada[x], enc_text+x, tmp);
-			tmp--;
-			x++;
-	} while (tmp > 0);
-
-	printf("\nEncrypted Message (hexa) FULL:\t");
-
-	for (x=0; x < textlen; x++)
-	printf("%2.X ",msg_cifrada[x]);
-
-	//Loop to decrypt 16 to 16 bytes
-	x=0;
-	tmp=textlen;
- 	do {
-		AES_decrypt(msg_cifrada+x, dec_text+x, &aeskey);
-		memcpy(&msg_decifrada[x], dec_text+x, tmp);
-
-		printf("\nMSGCIFRADA = ###%2.X###\t", *msg_cifrada+x);
-		printf("\n##DEC_TEXT = ###%2.X\t", *dec_text);
-
-		tmp--;
-		x++;
-	} while (tmp > 0);
-
-	printf("\nDecrypted Message (hexa) FULL:\t");
-	for (x=0; x < textlen; x++)
-		printf("%2.X ",msg_decifrada[x]);
-/*
- * * Finish loop to encrypt/decrypt
- */
-
-	printf("%s ", "\n\nA PARTIR DAQUI TA WORKANDO\n");
 
 	printf("\nOriginal Message (string):\t");
-	printf("%s ",text);
+	printf("%s ", text);
 
 	printf("\nOriginal Message (hexa):\t");
-	for (x=0;*(text+x)!=0x00; x++)
-		printf("%2.X ",*(text+x));
+	print_hex(text);
 
 	printf("\nEncrypted Message (hexa):\t");
-	for (x = 0; *(enc_text+x) != 0x00; x++)
-		printf("%2.X ",*(enc_text+x));
+	print_hex(msg_cifrada);
 
 	printf("\nDecrypted Message (hexa):\t");
-	for (x=0;*(dec_text+x)!=0x00; x++)
-		printf("%2.X ",*(dec_text+x));
+	print_hex(msg_decifrada);
 
 	printf("\n");
 
-	if (dec_text != NULL)
-		free(dec_text);
-	if (enc_text != NULL)
-		free(enc_text);
 	if (text != NULL)
 		free(text);
 	if (msg_cifrada != NULL)
